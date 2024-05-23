a4267f642d038325a28d1ace685c89c33a5ca4ca
WeihengOh <ohweiheng@gmail.com>
2024-05-23 00:56:22 -0400

    Added touchscreen click and mouseclick to generate color

--- script.js
+++ script.js
@@ -6,18 +6,21 @@ function changeHexcode(hexcode) {
     document.getElementById("mainLine").innerHTML = hexcode;
 }
 
-/* functions called in index.html,  generates a random green and and random red color*/
+/* functions called to generate a random green and and random red color*/
 function generateGreen() {
     green = randomColor(138, 154, 91, 20);
     changeBackground(green);    
     changeHexcode(green);
+    document.getElementById("subLine").innerHTML = null;
 }
 function generateRed() {
     red = randomColor(136, 8, 8, 30);
     changeBackground(red);
     changeHexcode(red);
+    document.getElementById("subLine").innerHTML = null;
 }
 
+/* base color generation function */
 function randomColor(r, g, b, range) {
     const initial = [r, g, b];
     var newColor = [0, 0, 0];
@@ -31,7 +34,7 @@ function randomColor(r, g, b, range) {
     return rgbToHex(newColor[0], newColor[1], newColor[2]);
 }
 
-
+/* helper functions for color generation and boundary checks */
 function componentToHex(c) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
@@ -39,17 +42,28 @@ function componentToHex(c) {
 function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
-
-window.addEventListener("keydown", checkKeyPressed, false);
-
-function checkKeyPressed(evt) {
-    if (evt.keyCode == "32") {
-        generateGreen();
-        document.getElementById("subLine").innerHTML = null;
-    }
-}
-
 function inRange(value) {
     if (value < 1 || value > 255) return false;
     return true;
 }
+
+/* change the background */
+window.addEventListener("keydown", checkSpacePressed, false);
+function checkSpacePressed(evt) {
+    if (evt.keyCode == "32") {
+        generateGreen();
+    }
+}
+
+/* define elements for checkMouseClick below */
+window.addEventListener("click", checkMousePress, false);
+const nameButton = document.getElementById("redButton");
+/* if mouseClick and mouse is not over any buttons, generateGreen */
+function checkMousePress(evt) {
+    var target = (evt.target);
+    if(target.id !== nameButton.id) {
+        generateGreen();
+    }
+};
+
+
